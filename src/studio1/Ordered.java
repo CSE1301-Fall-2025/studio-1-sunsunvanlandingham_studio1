55191bd83721928eb26f24604e9e034fed630348
孙若琰 <ruoyansun@sunruoyandeMacBook-Pro.local>
2025-09-03 14:09:00 -0500

    studio 1 finished

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,10 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		boolean ascending = (x<y)&&(y<z);
+		boolean descending = (x>y)&&(y>z);
+		boolean isOrdered = ascending || descending;
+		System.out.println(isOrdered);
 
 	}
 
